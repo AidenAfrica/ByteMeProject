3a7faf1e4b2476c1bb13bdad0528896f330932c3
padifalasanerd <163738111+padifalas@users.noreply.github.com>
2024-07-12 16:34:43 +0200

    Intro Dialogue Scene Added

---
+++ Assets/Scripts/IntroDialogue.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement; 
+public class IntroDialogue : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public float wordSpeed;
+    public GameObject continueButton;
+    public GameObject startGameButton; 
+
+    private bool isTyping = false;
+
+    void Start()
+    {
+        dialoguePanel.SetActive(true); 
+        dialogueText.text = "";
+        continueButton.SetActive(false);
+        startGameButton.SetActive(false); 
+        StartCoroutine(Typing()); 
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        continueButton.SetActive(false);
+        startGameButton.SetActive(false); 
+        dialoguePanel.SetActive(false); 
+    }
+
+    IEnumerator Typing()
+    {
+        Debug.Log("Starting Typing dialogue.");
+        dialogueText.text = "";
+        isTyping = true;
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+        isTyping = false;
+        Debug.Log("Finished Typing dialogue.");
+        continueButton.SetActive(true);
+    }
+
+    public void NextLine()
+    {
+        continueButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+            startGameButton.SetActive(true); 
+        }
+    }
+
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LevelDesign"); 
+    }
+}
